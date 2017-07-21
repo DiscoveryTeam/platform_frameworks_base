9194530f1c239bb52180e0724f226c7ddf2972ba
Kensuke Matsui <kensuke.matsui@sony.com>
2017-07-21 17:35:10 +0900

    Fix NPE in StatusBar#onBackPressed()
    StatusBar.mKeyguardUserSwitcher is instantiated
    only if multi user function is enabled.
    But mKeyguardUserSwitcher.hideIfNotSimple() is invoked
    without null check in StatusBar#onBackPressed().
    This causes NPE on multi user disabled devices.
    
    Fixes: 65391930
    Test: manual - tap Back key on lockscreen
    
    Change-Id: I528823fa832ee5096d73dea660997fc276d86c5c
    
    Signed-off-by: xyyx <xyyx@mail.ru>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4487,7 +4487,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             animateCollapsePanels();
             return true;
         }
-        if (mKeyguardUserSwitcher.hideIfNotSimple(true)) {
+        if (mKeyguardUserSwitcher != null && mKeyguardUserSwitcher.hideIfNotSimple(true)) {
             return true;
         }
         return false;
